2d02c93c1a54b3f86d2af9f2250bbbea8b17bfa9
Jorge Ossa <jorgehossa@gmail.com>
2021-02-01 22:15:22 -0500

    Add table and data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {Table} from 'antd'
 
 function App() {
+  const [data, setData] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageSize: null,
+    page: null,
+    total: null,
+  });
+
+  useEffect(() => {
+    fetch('https://api.datos.gob.mx/v2/sinaica')
+      .then(res => res.json())
+      .then(data => {
+        setPagination(data.pagination);
+        setData(data.results);
+      });
+  }, [data]);
+
+  let columns = [
+    {
+      title: 'City',
+      dataIndex: 'city',
+    },
+    {
+      title: 'State',
+      dataIndex: 'state',
+    },
+    {
+      title: 'Valororig',
+      dataIndex: 'valororig',
+    },
+    {
+      title: 'Parametro',
+      dataIndex: 'parametro',
+    }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} dataSource={data} />
     </div>
   );
 }
